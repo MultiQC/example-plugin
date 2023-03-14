55cc0c4b4a5a863f5cf534807b992c7ce9d2352e
lars20070 <l.nilse@dunelm.org.uk>
2023-03-14 09:36:39 +0100

    change test files, black formatting

--- example_plugin/modules/my_example/my_example.py
+++ example_plugin/modules/my_example/my_example.py
@@ -11,57 +11,57 @@ from multiqc.plots import linegraph
 from multiqc.modules.base_module import BaseMultiqcModule
 
 # Initialise the main MultiQC logger
-log = logging.getLogger('multiqc')
+log = logging.getLogger("multiqc")
+
 
 class MultiqcModule(BaseMultiqcModule):
-
     def __init__(self):
 
         # Halt execution if we've disabled the plugin
-        if config.kwargs.get('disable_plugin', True):
+        if config.kwargs.get("disable_plugin", True):
             return None
 
         # Initialise the parent module Class object
         super(MultiqcModule, self).__init__(
-            name = 'My Example',
-            target = "my_example",
-            anchor = 'my_example',
-            href = 'https://github.com/MultiQC/example-plugin',
-            info = " is an example module to show how the MultiQC plugin system works."
+            name="My Example",
+            target="my_example",
+            anchor="my_example",
+            href="https://github.com/MultiQC/example-plugin",
+            info=" is an example module to show how the MultiQC plugin system works.",
         )
 
         # Find and load any input files for this module
         self.my_example_data = dict()
-        for f in self.find_log_files('my_example/key_value_pairs'):
-            self.my_example_data[f['s_name']] = dict()
-            for l in f['f'].splitlines():
+        for f in self.find_log_files("my_example/key_value_pairs"):
+            self.my_example_data[f["s_name"]] = dict()
+            for l in f["f"].splitlines():
                 key, value = l.split(None, 1)
-                self.my_example_data[f['s_name']][key] = value
+                self.my_example_data[f["s_name"]][key] = value
 
-        for f in self.find_log_files('my_example/mzml'):
-            self.my_example_data[f['s_name']] = dict()
-            for l in f['f'].splitlines():
+        for f in self.find_log_files("my_example/mzml"):
+            self.my_example_data[f["s_name"]] = dict()
+            for l in f["f"].splitlines():
                 key, value = l.split(None, 1)
-                self.my_example_data[f['s_name']][key] = value
+                self.my_example_data[f["s_name"]][key] = value
 
-        for f in self.find_log_files('my_example/fragpipe'):
-            self.my_example_data[f['s_name']] = dict()
-            for l in f['f'].splitlines():
+        for f in self.find_log_files("my_example/fragpipe"):
+            self.my_example_data[f["s_name"]] = dict()
+            for l in f["f"].splitlines():
                 key, value = l.split(None, 1)
-                self.my_example_data[f['s_name']][key] = value
+                self.my_example_data[f["s_name"]][key] = value
 
-        for f in self.find_log_files('my_example/fdr'):
-            self.my_example_data[f['s_name']] = dict()
-            for l in f['f'].splitlines():
+        for f in self.find_log_files("my_example/fdr"):
+            self.my_example_data[f["s_name"]] = dict()
+            for l in f["f"].splitlines():
                 key, value = l.split(None, 1)
-                self.my_example_data[f['s_name']][key] = value
+                self.my_example_data[f["s_name"]][key] = value
 
         self.my_example_plot_data = dict()
-        for f in self.find_log_files('my_example/plot_data'):
-            self.my_example_plot_data[f['s_name']] = dict()
-            for l in f['f'].splitlines():
+        for f in self.find_log_files("my_example/plot_data"):
+            self.my_example_plot_data[f["s_name"]] = dict()
+            for l in f["f"].splitlines():
                 key, value = l.split(None, 1)
-                self.my_example_plot_data[f['s_name']][float(key)] = float(value)
+                self.my_example_plot_data[f["s_name"]][float(key)] = float(value)
 
         # Filter out samples matching ignored sample names
         self.my_example_data = self.ignore_samples(self.my_example_data)
@@ -74,32 +74,32 @@ class MultiqcModule(BaseMultiqcModule):
         log.info("Found {} reports".format(len(self.my_example_data)))
 
         # Write parsed report data to a file
-        self.write_data_file(self.my_example_data, 'multiqc_my_example')
+        self.write_data_file(self.my_example_data, "multiqc_my_example")
 
         # Add a number to General Statistics table
         headers = OrderedDict()
-        headers['data_key'] = {
-            'title': '# Things',
-            'description': 'An important number showing something useful.',
-            'min': 0,
-            'scale': 'RdYlGn-rev',
-            'format': '{:,.0f}'
+        headers["data_key"] = {
+            "title": "# Things",
+            "description": "An important number showing something useful.",
+            "min": 0,
+            "scale": "RdYlGn-rev",
+            "format": "{:,.0f}",
         }
         self.general_stats_addcols(self.my_example_data, headers)
 
         # Create line plot
         pconfig = {
-            'id': 'my_example_plot',
-            'title': 'My Example: An example plot',
-            'ylab': '# Other Things',
-            'xlab': 'Some Values'
+            "id": "my_example_plot",
+            "title": "My Example: An example plot",
+            "ylab": "# Other Things",
+            "xlab": "Some Values",
         }
         line_plot_html = linegraph.plot(self.my_example_plot_data, pconfig)
 
         # Add a report section with the line plot
         self.add_section(
-            description = 'This plot shows some numbers, and how they relate.',
-            helptext = '''
+            description="This plot shows some numbers, and how they relate.",
+            helptext="""
             This longer description explains what exactly the numbers mean
             and supports markdown formatting. This means that we can do _this_:
 
@@ -109,6 +109,6 @@ class MultiqcModule(BaseMultiqcModule):
 
             Doesn't matter if this is copied from documentation - makes it
             easier for people to find quickly.
-            ''',
-            plot = line_plot_html
+            """,
+            plot=line_plot_html,
         )
--- setup.py
+++ setup.py
@@ -8,49 +8,47 @@ For more information about MultiQC, see http://multiqc.info
 
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = "0.1"
 
 setup(
-    name = 'multiqc_example_plugin',
-    version = version,
-    author = 'Phil Ewels',
-    author_email = 'phil.ewels@scilifelab.se',
-    description = "Example MultiQC plugin",
-    long_description = __doc__,
-    keywords = 'bioinformatics',
-    url = 'https://github.com/MultiQC/example-plugin',
-    download_url = 'https://github.com/MultiQC/example-plugin/releases',
-    license = 'MIT',
-    packages = find_packages(),
-    include_package_data = True,
-    install_requires = [
-        'multiqc'
-    ],
-    entry_points = {
-        'multiqc.modules.v1': [
-            'my_example = example_plugin.modules.my_example:MultiqcModule',
+    name="multiqc_example_plugin",
+    version=version,
+    author="Phil Ewels",
+    author_email="phil.ewels@scilifelab.se",
+    description="Example MultiQC plugin",
+    long_description=__doc__,
+    keywords="bioinformatics",
+    url="https://github.com/MultiQC/example-plugin",
+    download_url="https://github.com/MultiQC/example-plugin/releases",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["multiqc"],
+    entry_points={
+        "multiqc.modules.v1": [
+            "my_example = example_plugin.modules.my_example:MultiqcModule",
         ],
-        'multiqc.cli_options.v1': [
-            'disable_plugin = example_plugin.cli:disable_plugin'
+        "multiqc.cli_options.v1": [
+            "disable_plugin = example_plugin.cli:disable_plugin"
         ],
-        'multiqc.hooks.v1': [
-            'execution_start = example_plugin.custom_code:example_plugin_execution_start'
-        ]
+        "multiqc.hooks.v1": [
+            "execution_start = example_plugin.custom_code:example_plugin_execution_start"
+        ],
     },
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-        'Programming Language :: Python',
-        'Programming Language :: JavaScript',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Topic :: Scientific/Engineering :: Visualization',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: JavaScript",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Visualization",
     ],
 )
